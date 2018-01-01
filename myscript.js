4a158531c89cf0d71e6646b6c2faea1e8deb5304
Bhavin Vadalia <bhavinv@outlook.com>
2018-01-01 08:27:59 +0530

    Updated to v7 of the API

--- myscript.js
+++ myscript.js
@@ -1,27 +1,26 @@
 (function(){
 
-var app = angular.module("Myapp",[]);
-
-var myctrl = function($scope,$http,$location){
-	$scope.mytext = "hello";
-	$scope.new_var=null;
-	$scope.zorm=function(){
+var app = angular.module('myapp', []);
+var myctrl = function($scope,$http){
+  
+  $scope.getresults=function(){
 		var config = {headers: {
-             'Ocp-Apim-Subscription-Key': '851a4ef093104471b8a58b9e909a4f7a'
+             'Ocp-Apim-Subscription-Key': 'ad5ee2fee96a4594ab5a899b73701bfc'
         }};
 		var myq = $scope.myquery;
 		myq=encodeURIComponent(myq.trim());$scope.myq=myq;
-		var urltogo = 'https://api.cognitive.microsoft.com/bing/v5.0/search?q='+myq+'&responseFilter=Webpages&count=20&offset=0&mkt=en-in';
+		var urltogo = 'https://api.cognitive.microsoft.com/bing/v7.0/search?q='+myq+'&responseFilter=Webpages&count=20&offset=0&mkt=en-in';
+    
 		$http.get(urltogo, config)
 		.then(function(response){
 			$scope.res_resp=response.data.webPages.value;
 			$scope.new_var=true;
+      
 			})
 		
-		};
-	
-	
+};
 };
 
-app.controller("myctrl", myctrl);
+app.controller('myctrl', myctrl);
+
 })();
